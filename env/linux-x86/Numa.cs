b2be0bd05b5d975c6540ff030b07daff2004a4f0
Samuel Chassot <samuel.chassot@epfl.ch>
2020-02-19 16:43:35 +0100

    add numa.cs with empy def

---
+++ env/linux-x86/Numa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace env.linuxx86
+{
+    public class Numa
+    {
+        public static bool tn_numa_is_current_node(UInt64 node)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool tn_numa_get_addr_node(UIntPtr addr)//, uint64_t* out_node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
