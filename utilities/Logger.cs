58b16072570789b92d143899eb69076b191251d2
Samuel Chassot <samuel.chassot@epfl.ch>
2020-02-19 16:43:59 +0100

    add utilities project, add logger with def

---
+++ utilities/Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace utilities
+{
+    // Logging utilities.
+    // There are 4 log levels:
+    // - 0: No logging at all
+    // - 1: Logging important information, like "initialization complete"
+    // - 2: Logging debugging information, like "some operation failed, although the overall system may be fine"
+    // - 3: Logging everything, like "wrote value 0xAB to register CD of device 3"
+    public class Logger
+    {
+        private byte level;
+        public Logger(byte level)
+        {
+            this.level = level;
+        }
+        public void Verbose(string msg)
+        {
+            if (level >= 3)
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("VERBOSE");
+                Console.WriteLine(msg);
+                Console.Error.Flush();
+                Console.SetOut(Console.Out);
+            }
+        }
+
+        public void Debug(string msg)
+        {
+            if (level >= 2)
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("DEBUG");
+                Console.WriteLine(msg);
+                Console.Error.Flush();
+                Console.SetOut(Console.Out);
+            }
+        }
+
+        public void Info(string msg)
+        {
+            if (level >= 1)
+            {
+                Console.SetOut(Console.Error);
+                Console.WriteLine("INFO");
+                Console.WriteLine(msg);
+                Console.Error.Flush();
+                Console.SetOut(Console.Out);
+            }
+        }
+    }
+}
