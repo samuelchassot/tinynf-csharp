f596c36d301499d003f30af4bcb585e3050122e5
Samuel Chassot <samuel.chassot@epfl.ch>
2020-02-25 18:06:06 +0100

    rename the cwrapper to MacrosCstVal.c

--- C#-driver/cwrapper/CWrapper.c
+++
@@ -1,8 +0,0 @@
-#include "CWrapper.h"
-#include <stdio.h>
-#include <unistd.h>
-
-//memory.c functions
-int cst_sc_pagesize(){
-    return _SC_PAGESIZE;
-}
---
+++ C#-driver/tinynf-sam/utilities/MacrosValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Utilities
+{
+
+    public enum ExternalMacrosDefinedConstantsName
+    {
+        _SC_PAGESIZE = 1,
+        PROT_READ = 2,
+        PROT_WRITE = 3,
+        MAP_HUGETLB = 4,
+    }
+
+    public static class EnumExtensions
+    {
+        [DllImport(@"MacrosCstVal.so")]
+        private static extern int getSystemCstValues(int id);
+
+        public static int getValue(this ExternalMacrosDefinedConstantsName val)
+        {
+            return getSystemCstValues((int)val);
+        }
+    }
+}
