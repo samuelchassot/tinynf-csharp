5201255f5d116ba7a0d2bce12803a43031c081a2
Samuel Chassot <samuel.chassot@epfl.ch>
2020-04-16 18:13:07 +0200

    try to disable Quick JIT and tired JIT

--- code/build.sh
+++ code/build.sh
@@ -1,5 +1,5 @@
 cd tinynf-sam
-dotnet build --runtime linux-x64 -c Release
+COMPlus_TieredCompilation=0 COMPlus_TC_QuickJit=0 dotnet build --runtime linux-x64 -c Release
 cd ..
 
 cp -r tinynf-sam/tinynf-sam/bin/Debug/netcoreapp3.1/linux-x64/ ./build
