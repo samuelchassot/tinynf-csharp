4837dc42f4f7dd987806c253426608455ba75047
Samuel Chassot <samuel.chassot@epfl.ch>
2020-04-18 11:17:33 +0200

    reenable quick jit and tiered jit

--- code/build.sh
+++ code/build.sh
@@ -4,7 +4,7 @@ cd tinynf-sam
 dotnet clean -c Release --runtime linux-x64
 dotnet clean -c Debug --runtime linux-x64
 
-COMPlus_TieredCompilation=0 COMPlus_TC_QuickJit=0 dotnet build --runtime linux-x64 -c $1
+dotnet build --runtime linux-x64 -c $1
 cd ..
 
 cp -r tinynf-sam/tinynf-sam/bin/$1/netcoreapp3.1/linux-x64/* ./build/
