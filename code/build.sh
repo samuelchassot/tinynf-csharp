f26a2cc50fdf4ee658625ecf31a411cde004a7b1
Samuel Chassot <samuel.chassot@epfl.ch>
2020-04-18 15:49:30 +0200

    try optimize option

--- code/build.sh
+++ code/build.sh
@@ -4,7 +4,7 @@ cd tinynf-sam
 dotnet clean -c Release --runtime linux-x64
 dotnet clean -c Debug --runtime linux-x64
 
-dotnet build --runtime linux-x64 -c $1
+dotnet build --runtime linux-x64 -c $1 -optimize+
 cd ..
 
 cp -r tinynf-sam/tinynf-sam/bin/$1/netcoreapp3.1/linux-x64/* ./build/
