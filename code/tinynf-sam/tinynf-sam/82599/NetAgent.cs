938f96c865d2927c336838cc45bedea9df83f599
samuelchassot <samuel.chassot@epfl.ch>
2020-05-30 13:35:12 +0200

    receive not opti, process is

--- code/tinynf-sam/tinynf-sam/82599/NetAgent.cs
+++ code/tinynf-sam/tinynf-sam/82599/NetAgent.cs
@@ -337,6 +337,7 @@ namespace tinynf_sam
             return true;
         }
 
+        [MethodImpl(MethodImplOptions.NoOptimization)]
         public (bool ok, int packetLength, UIntPtr packetAddr) Receive()
         {
             // Since descriptors are 16 bytes, the index must be doubled
@@ -450,7 +451,7 @@ namespace tinynf_sam
         // --------------
         // High-level API
         // --------------
-        [MethodImpl(MethodImplOptions.NoOptimization)]
+        
         public void Process(Func<int, UIntPtr, bool[], int> packetHandler)
         {
             (bool ok, int packetLength, UIntPtr packetPtr) = this.Receive();
