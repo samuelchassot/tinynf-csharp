243d1e63ba4ac853e8c628f963f8596fbf220cd9
samuelchassot <samuel.chassot@epfl.ch>
2020-06-04 17:52:22 +0200

    reformat and make .h clean

--- code/cwrapper/FunctionsWrapper.c
+++ code/cwrapper/FunctionsWrapper.c
@@ -1,12 +1,6 @@
 #include "FunctionsWrapper.h"
 
-#include <sys/syscall.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <sys/io.h>
+
 
 #ifndef MAP_HUGE_SHIFT
 #define MAP_HUGE_SHIFT 26
@@ -15,27 +9,30 @@
 //Most of the functions here are just wrapped because they cannot be called from C# directly
 
 long get_mempolicy(int *mode, unsigned long *nodemask,
-                   unsigned long maxnode, void *addr,
-                   unsigned long flags)
+				   unsigned long maxnode, void *addr,
+				   unsigned long flags)
 {
-    return syscall(SYS_get_mempolicy, mode, nodemask, maxnode, addr, flags);
+	return syscall(SYS_get_mempolicy, mode, nodemask, maxnode, addr, flags);
 }
 
-int getcpu(unsigned *cpu, unsigned *node){
-    return syscall(SYS_getcpu, cpu, node, NULL);
+int getcpu(unsigned *cpu, unsigned *node)
+{
+	return syscall(SYS_getcpu, cpu, node, NULL);
 }
 
-uintptr_t virt_to_phys_mem(uintptr_t addr, unsigned long size){
-    //checks are already performed in C#
-    int mem_fd = open("/dev/mem", O_SYNC | O_RDWR);
-	if (mem_fd == -1) {
-		return (uintptr_t) (void*)-1;
+uintptr_t virt_to_phys_mem(uintptr_t addr, unsigned long size)
+{
+	//checks are already performed in C#
+	int mem_fd = open("/dev/mem", O_SYNC | O_RDWR);
+	if (mem_fd == -1)
+	{
+		return (uintptr_t)(void *)-1;
 	}
-    void* mapped = mmap(
+	void *mapped = mmap(
 		// No specific address
 		NULL,
 		// Size of the mapping (cast OK because we checked above)
-		(size_t) size,
+		(size_t)size,
 		// R/W page
 		PROT_READ | PROT_WRITE,
 		// Send updates to the underlying "file"
@@ -43,22 +40,23 @@ uintptr_t virt_to_phys_mem(uintptr_t addr, unsigned long size){
 		// /dev/mem
 		mem_fd,
 		// Offset is the address (cast OK because we checked above)
-		(off_t) addr
-	);
-    //and checks will be performed in C#
-    return (uintptr_t) mapped;
+		(off_t)addr);
+	//and checks will be performed in C#
+	return (uintptr_t)mapped;
 }
 
-int mem_virt_to_phys(const uintptr_t page, const uintptr_t map_offset, uint64_t* outMetadata)
+int mem_virt_to_phys(const uintptr_t page, const uintptr_t map_offset, uint64_t *outMetadata)
 {
 
 	const int map_fd = open("/proc/self/pagemap", O_RDONLY);
-	if (map_fd < 0) {
+	if (map_fd < 0)
+	{
 		//TN_DEBUG("Could not open the pagemap");
 		return 1;
 	}
 
-	if (lseek(map_fd, (off_t) map_offset, SEEK_SET) == (off_t) -1) {
+	if (lseek(map_fd, (off_t)map_offset, SEEK_SET) == (off_t)-1)
+	{
 		//TN_DEBUG("Could not seek the pagemap");
 		close(map_fd);
 		return 2;
@@ -67,7 +65,8 @@ int mem_virt_to_phys(const uintptr_t page, const uintptr_t map_offset, uint64_t*
 	uint64_t metadata;
 	const ssize_t read_result = read(map_fd, &metadata, sizeof(uint64_t));
 	close(map_fd);
-	if (read_result != sizeof(uint64_t)) {
+	if (read_result != sizeof(uint64_t))
+	{
 		//TN_DEBUG("Could not read the pagemap");
 		return 3;
 	}
@@ -76,30 +75,35 @@ int mem_virt_to_phys(const uintptr_t page, const uintptr_t map_offset, uint64_t*
 }
 
 //need to define them like that because they are defined as macros so cannot be called directly from C#
-void outlCustom(unsigned int value, unsigned short int port){
-    outl(value, port);
+void outlCustom(unsigned int value, unsigned short int port)
+{
+	outl(value, port);
 }
 
-void outbCustom(unsigned char value, unsigned short int port){
-    outb(value, port);
+void outbCustom(unsigned char value, unsigned short int port)
+{
+	outb(value, port);
 }
 
-unsigned int inlCustom(unsigned short int port){
-    return inl(port);
+unsigned int inlCustom(unsigned short int port)
+{
+	return inl(port);
 }
 
-int numberOfTrailingZeros(unsigned long n){
-    return __builtin_ctzll(n);
+int numberOfTrailingZeros(unsigned long n)
+{
+	return __builtin_ctzll(n);
 }
 
-int get_sc_pagesize(){
+int get_sc_pagesize()
+{
 	return _SC_PAGESIZE;
 }
 
 uintptr_t tn_mem_allocate_C(const uint64_t size, const uint64_t HUGEPAGE_SIZE, const int HUGEPAGE_SIZE_POWER)
 {
 	// http://man7.org/linux/man-pages//man2/munmap.2.html
-	void* page = mmap(
+	void *page = mmap(
 		// No specific address
 		NULL,
 		// Size of the mapping
@@ -113,18 +117,18 @@ uintptr_t tn_mem_allocate_C(const uint64_t size, const uint64_t HUGEPAGE_SIZE, c
 		// Required on MAP_ANONYMOUS
 		-1,
 		// Required on MAP_ANONYMOUS
-		0
-	);
-	if (page == MAP_FAILED) {
+		0);
+	if (page == MAP_FAILED)
+	{
 		//TN_DEBUG("Allocate mmap failed");
 		return 0;
 	}
 
-	uintptr_t addr = (uintptr_t) page;
+	uintptr_t addr = (uintptr_t)page;
 	return addr;
 }
 
 void tn_mem_free_C(const uintptr_t addr, const uint64_t HUGEPAGE_SIZE)
 {
-	munmap((void*) addr, (size_t)HUGEPAGE_SIZE);
+	munmap((void *)addr, (size_t)HUGEPAGE_SIZE);
 }
--- code/cwrapper/FunctionsWrapper.h
+++ code/cwrapper/FunctionsWrapper.h
@@ -1,3 +1,11 @@
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/io.h>
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -19,7 +27,15 @@ extern "C"
 
     //I omitted the last argument ( struct getcpu_cache *tcache ) because it is NULL anyway and I do not have the struct definition
     MODULE_API int getcpu(unsigned *cpu, unsigned *node);
-
+    MODULE_API uintptr_t virt_to_phys_mem(uintptr_t addr, unsigned long size);
+    MODULE_API int mem_virt_to_phys(const uintptr_t page, const uintptr_t map_offset, uint64_t *outMetadata);
+    MODULE_API void outlCustom(unsigned int value, unsigned short int port);
+    MODULE_API void outbCustom(unsigned char value, unsigned short int port);
+    MODULE_API unsigned int inlCustom(unsigned short int port);
+    MODULE_API int numberOfTrailingZeros(unsigned long n);
+    MODULE_API int get_sc_pagesize();
+    MODULE_API uintptr_t tn_mem_allocate_C(const uint64_t size, const uint64_t HUGEPAGE_SIZE, const int HUGEPAGE_SIZE_POWER);
+    MODULE_API void tn_mem_free_C(const uintptr_t addr, const uint64_t HUGEPAGE_SIZE);
 #ifdef __cplusplus
 }
 #endif
