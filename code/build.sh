56fc7b76cbd60a3421dfa85f47c1682717e9e130
samuelchassot <samuel.chassot@epfl.ch>
2020-06-12 16:07:49 +0200

    rename tinynf-sam into tinynf-csharp

--- code/build.sh
+++ code/build.sh
@@ -1,14 +1,14 @@
 rm -rf build
 mkdir build
 
-cd tinynf-sam
+cd tinynf-csharp
 dotnet clean -c Release
 dotnet clean -c Debug
 
 dotnet build --runtime linux-x64 -c $1
 cd ..
 
-cp -r tinynf-sam/tinynf-sam/bin/$1/netcoreapp3.1/linux-x64/* ./build/
+cp -r tinynf-csharp/tinynf-csharp/bin/$1/netcoreapp3.1/linux-x64/* ./build/
 
 cd cwrapper
 rm -f FunctionsWrapper.so
